c52c4479acbc9b271e2af6923a61f4dbf3295847
Sanjaykanwasi <sanjaykanwasi3@gmail.com>
2024-08-03 13:01:03 +0530

    new

---
+++ app.js
@@ -0,0 +1,20 @@
+// Display box
+const display = document.getElementById("display");
+
+// functions
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
